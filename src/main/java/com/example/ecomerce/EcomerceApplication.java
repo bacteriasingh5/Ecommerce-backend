b6daafa3f4cca9f5ad1a669757aa668f9f16eb59
bacteriasingh5 <bacteriasingh5@gmail.com>
2025-03-08 04:14:07 +0530

    added auto configration

--- src/main/java/com/example/ecomerce/EcomerceApplication.java
+++ src/main/java/com/example/ecomerce/EcomerceApplication.java
@@ -1,9 +1,11 @@
 package com.example.ecomerce;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@AutoConfiguration
 public class EcomerceApplication {
 
 	public static void main(String[] args) {
